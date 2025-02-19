e9b655db7178e819243dd716b046c9bab1036163
AdzhanSerik <adzhan.serik@gmail.com>
2025-02-19 20:01:39 +0500

    dasdsad

---
+++ back-end/src/models/usersModel.js
@@ -0,0 +1,31 @@
+const { DataTypes } = require('sequelize')
+const sequelize = require('../config/db')
+
+const User = sequelize.define(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            autoIncrement: true,
+            primaryKey: true,
+            allowNull: false
+        },
+        email: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            unique: true,
+            validate: { isEmail: true }
+        },
+        first_name: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        password: {
+            type: DataTypes.STRING,
+            allowNull: false
+        }
+    },
+    {
+        tableName: 'users'
+    }
+
+)
