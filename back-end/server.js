ca1b33caec9cd427236324477ed8a4db0465e614
AdzhanSerik <adzhan.serik@gmail.com>
2025-02-19 20:13:11 +0500

    dasdsad

--- back-end/server.js
+++ back-end/server.js
@@ -1,13 +1,15 @@
 require('dotenv').config()
 const sequelize = require('./src/config/db')
-
 const express = require('express')
 const cors = require('cors')
+const userRoutes = require('./src/routes/userRoutes')
 
 const app = express()
 
 app.use(express.json())
 app.use(cors())
+
+app.use('/api/userRoutes', userRoutes)
 
 const PORT = process.env.PORT || 3000
 
