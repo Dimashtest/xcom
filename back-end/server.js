9900c48cd9b9792606ee2fd1ee4d88c458165c47
AdzhanSerik <adzhan.serik@gmail.com>
2025-02-19 19:47:30 +0500

    dasdsad

--- back-end/server.js
+++ back-end/server.js
@@ -1,4 +1,5 @@
 require('dotenv').config()
+const sequelize = require('./src/config/db')
 
 const express = require('express')
 const cors = require('cors')
@@ -9,6 +10,9 @@ app.use(express.json())
 app.use(cors())
 
 const PORT = process.env.PORT || 3000
+
+
+
 
 app.listen(PORT, () => {
     console.log(`Server starten on port http://localhost:${PORT}`);
